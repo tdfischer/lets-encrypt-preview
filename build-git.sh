b6d736e87849f91c5320d85845ea3061b3e7604c
Torrie Fischer <tdfischer@hackerbots.net>
2015-04-26 16:10:46 -0700

    Write a quick rpmspec and script to package lets-encrypt

---
+++ build-git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+tag=$(git describe --always)
+archive=lets-encrypt-$tag.tar
+
+git archive --prefix=lets-encrypt-$tag/ master > $archive
+mkdir lets-encrypt-$tag/ 2>/dev/null
+sed -e "s/<GIT-TAG>/$tag/" < lets-encrypt.spec.in > lets-encrypt-$tag/lets-encrypt.spec
+tar rf $archive lets-encrypt-$tag/
+cat $archive | xz > $archive.xz
+rpmbuild -tb $archive
